49a355e1a82fef596c1bcbbed891fac3517e6e24
JJunio <junio.araujo01@yahoo.com>
2021-10-14 23:28:37 -0300

    Teste controller person

---
+++ src/br/ifpe/edu/agendamento/controller/ControllerPerson.java
@@ -0,0 +1,43 @@
+package br.ifpe.edu.agendamento.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.hibernate.Session;
+
+import br.ifpe.edu.agendamento.model.dao.DAOPostgreSQL;
+import br.ifpe.edu.agendamento.model.entity.Person;
+
+/**
+ * Servlet implementation class ControllerPerson
+ */
+@WebServlet("/ControllerPerson")
+public class ControllerPerson extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public ControllerPerson() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset-UTF-8");
+		System.out.println("To aqui");
+		
+		Person pessoa = new Person("123", "Teste", "000000", "carai@hotmail.com");
+		DAOPostgreSQL.getInstance();
+		Session session = DAOPostgreSQL.startTransaction();
+		session.save(pessoa);
+		
+	}
+
+}
